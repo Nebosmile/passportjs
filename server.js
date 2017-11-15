49bbaa3712123c125ed55f65a6cdd1c42ee6667b
Pavel <akulov_pavel88@mail.ru>
2017-11-15 17:54:20 +0200

    opent default template

---
+++ server.js
@@ -0,0 +1,25 @@
+const Koa = require('koa');
+const path = require('path');
+const fs = require('fs');
+const config = require('./config.js');
+
+
+
+var app = new Koa();
+app.use(async (ctx, next) => {
+  // in the future we'll extend this
+  ctx.set('Access-Control-Allow-Origin', '*');
+
+  await next();
+});
+
+const handlers = fs.readdirSync('./handlers').sort();
+handlers.forEach((handler)=>require('./handlers/'+ handler).init(app));
+
+
+
+// console.log(handlers);
+app.use(require('./routers').routes());
+
+
+app.listen(config.port)
